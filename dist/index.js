56fe638ee2326b7bdeec3d503b3a2db00ea3ac22
ANU7MADHAV <anumadhavan888@gmail>
2024-06-26 22:59:35 +0530

    Integrated bullmq board ui

--- dist/index.js
+++ dist/index.js
@@ -4,18 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const bullBoardUiConfig_1 = __importDefault(require("./config/bullBoardUiConfig"));
 const serverConfig_1 = __importDefault(require("./config/serverConfig"));
 const sampleQueueProducer_1 = __importDefault(require("./producers/sampleQueueProducer"));
 const routes_1 = __importDefault(require("./routes"));
 const sampleWorkers_1 = __importDefault(require("./workers/sampleWorkers"));
 const app = (0, express_1.default)();
 app.use("/api", routes_1.default);
+app.use("/dashboard", bullBoardUiConfig_1.default.getRouter());
 app.listen(serverConfig_1.default.PORT, () => {
     console.log(`Server started at *${serverConfig_1.default.PORT}`);
+    console.log(`BullBoard is running on: http://localhost:${serverConfig_1.default.PORT}/dashboard`);
     (0, sampleWorkers_1.default)("SampleQueue");
     (0, sampleQueueProducer_1.default)("SampleJob", {
-        name: "Sanket",
-        company: "Microsoft",
+        name: "Heloo",
+        company: "Google",
         position: "SDE 2 L61",
         locatiion: "Remote | BLR | Noida",
     }, 2);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import bullBoardAdapter from "./config/bullBoardUiConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
@@ -8,15 +9,19 @@ import SampleWorker from "./workers/sampleWorkers";
 const app = express();
 
 app.use("/api", apiRouter);
+app.use("/dashboard", bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *${serverConfig.PORT}`);
+  console.log(
+    `BullBoard is running on: http://localhost:${serverConfig.PORT}/dashboard`
+  );
   SampleWorker("SampleQueue");
   sampleQueueProducer(
     "SampleJob",
     {
-      name: "Sanket",
-      company: "Microsoft",
+      name: "Heloo",
+      company: "Google",
       position: "SDE 2 L61",
       locatiion: "Remote | BLR | Noida",
     },
